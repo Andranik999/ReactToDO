045c5a883415fa2e8d23a1b0b732e7816f87ac5d
Andranik999 <gyulasaryan97@gmail.com>
2021-05-11 19:14:37 +0400

    commit1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import { Footer } from "./Footer";
 import { TodoList } from "./TodoList.jsx";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState("");
+
+  const toggleTodoDone = id => {
+    const copiedTodos = [...todos];
+
+    copiedTodos.forEach(todo => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+    });
+
+    setTodos(copiedTodos);
+  };
+
+  const onRemove = id => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
 
   const handleChange = e => {
     setValue(e.target.value);
@@ -15,10 +33,33 @@ const App = () => {
       const trimmedText = value.trim();
 
       if (trimmedText) {
-        setTodos([trimmedText, ...todos]);
+        setTodos([{ title: trimmedText, done: false, id: uuidv4() }, ...todos]);
         setValue("");
       }
     }
+  };
+
+  const doneTodos = todos.filter(todo => todo.done);
+
+  const toggleAll = e => {
+    const toggledTodos = [...todos];
+    if (!doneTodos.length) {
+      toggledTodos.forEach(todo => {
+        todo.done = !todo.done;
+      });
+    }
+    if (doneTodos.length) {
+      const notDone = toggledTodos.filter(todo => !todo.done);
+      notDone.forEach(todo => {
+        todo.done = !todo.done;
+      });
+    }
+    if (doneTodos.length === toggledTodos.length) {
+      toggledTodos.forEach(todo => {
+        todo.done = !todo.done;
+      });
+    }
+    setTodos(toggledTodos);
   };
 
   return (
@@ -26,16 +67,23 @@ const App = () => {
       <section className="todo-app">
         <header className="header">
           <h1>todos</h1>
+          <div className="toggle-All" onClick={toggleAll}>
+            ⌄
+          </div>
           <input
             className="new-todo"
-            placeholder="What needs to be done"
+            placeholder="What needs to be done ?"
             onChange={handleChange}
             value={value}
             onKeyDown={handleKeyDown}
           />
         </header>
         <section className="main-todos">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            toggleTodoDone={toggleTodoDone}
+            onRemove={onRemove}
+          />
         </section>
       </section>
       <Footer />
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 
-export const TodoList = ({ todos, deleteTodo }) => {
+export const TodoList = ({ todos, onRemove, toggleTodoDone }) => {
   return (
-    <ul className="todo-list">
-      {todos.map((todo, index) => (
-        <li key={index}>
-          <input type="checkbox" className="check"></input>
-          <div className="remove-todo">X</div>
-          {todo}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <div className="check" onClick={() => toggleTodoDone(todo.id)}>
+              {todo.done && <span>✓</span>}
+            </div>
+            <div className="todo-title">{todo.title}</div>
+            <div className="remove-todo" onClick={() => onRemove(todo.id)}>
+              ✕
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <footer className="todo-info">
+        <span className="todo-count"></span>
+        <ul className="filter">
+          <li className="selectAll">
+            <span>All</span>
+          </li>
+          <li className="active-todos">
+            <span>Active</span>
+          </li>
+          <li className="completed-todos">
+            <span>Completed</span>
+          </li>
+          <li>
+            <span className="clearDone">Clear Completed</span>
+          </li>
+        </ul>
+      </footer>
+    </>
   );
 };
